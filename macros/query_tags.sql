82de781ee842f4abe5fb343e871ef4e63adaecb6
michael.g.fagan@gmail.com <michael.fagan@snyk.io>
2023-10-09 10:24:55 -0500

    Test

--- macros/query_tags.sql
+++ macros/query_tags.sql
@@ -37,7 +37,7 @@
 
         {%- endif -%}
 
-        {%- set cluster_by = cluster_by.split(", ") -%}
+        {%- set cluster_by = cluster_by.split(",") | map('trim') -%}
         
         {%- do query_tag.update(
             is_incremental=is_incremental(),
