dc2ba5edeefdd0862da9c292705970bb75d87f0a
michael.g.fagan@gmail.com <michael.fagan@snyk.io>
2023-10-09 09:43:07 -0500

    Test

--- macros/brute_force_cluster_list.sql
+++ macros/brute_force_cluster_list.sql
@@ -1,5 +1,7 @@
 {% macro brute_force_cluster_list(cluster_by) %}
 
+    {{ log("TEST", info=True) }}
+
     {% if ',' in cluster_by %}
 
         {% set cluster_by = cluster_by | replace('[', '') | replace(']', '') | replace("'", '') %}
@@ -9,6 +11,8 @@
     {% endif %}
 
     {% set cluster_by = cluster_by.split(", ")%}
+
+    {{ log(cluster_by, info=True) }}
 
     {{ return(cluster_by) }}
 
